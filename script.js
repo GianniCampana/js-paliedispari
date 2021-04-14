4b81f13111fe2ca716b616a8440d040d89a42885
GianniCampana <campanagianni97@gmail.com>
2021-04-14 18:03:43 +0200

    PALINDROMA

---
+++ script.js
@@ -0,0 +1,36 @@
+/* chiedo all utente una parola */
+
+var parola_utente = prompt("inserisci una parola");
+
+
+/* for(var i = parola_utente.length - 1; i>=0; i-- ){
+    palindroma += parola_utente[i];
+    if(parola_utente == palindroma){
+        document.getElementById("res").innerHTML="la parola " + parola_utente + " è palindroma: " + parola_utente + "=" + palindroma;
+    }else{
+        document.getElementById("res").innerHTML="la parola " + parola_utente + " non è palindroma: " + parola_utente + "!=" + palindroma;
+    }
+
+} */
+
+console.log(word(parola_utente));
+
+function word (par_ut){
+    var palindroma= "" ;
+    for(var i = par_ut.length - 1; i>=0; i-- ){
+        palindroma += par_ut[i];
+        if(parola_utente == palindroma){
+            document.getElementById("res").innerHTML= parola_utente + " è palindroma: " + palindroma ;
+        }else{
+            document.getElementById("res").innerHTML= parola_utente + " non è palindroma: " + palindroma ;
+        }
+    
+    }
+   return palindroma;
+
+}
+
+
+
+
+
